2945a6606e16b38ac3388c2b2b01874479e336d8
alldo08 <69772003+alldo08@users.noreply.github.com>
2026-02-21 01:41:51 -0300

    Update firebase-messaging-sw.js

--- site/static/firebase-messaging-sw.js
+++ site/static/firebase-messaging-sw.js
@@ -1,23 +1,24 @@
-// firebase-messaging-sw.js
+// static/firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-    apiKey: "AcbXX3KgvqD7B8Y4WjCu6yNx1Prfu5cNHz",
+const firebaseConfig = {
+    apiKey: "AIzaSyCGkSCkznXVBanRx5gDgeVSRS2mJU90UT8",
     projectId: "projeto-web-app-16adb",
     messagingSenderId: "1011089630045",
     appId: "1:1011089630045:web:3afc16a9817f11409c7eb4"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Opcional: Tratar mensagens em segundo plano
+// Captura notificações quando o app está fechado
 messaging.onBackgroundMessage((payload) => {
-    console.log('Mensagem recebida em segundo plano:', payload);
+    console.log('Mensagem em segundo plano:', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/static/icon.png'
+        icon: '/static/logo.png' // certifique-se que este ícone existe
     };
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
