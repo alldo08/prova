c4f4bc2adb4659e6dd81231da0c1a003ea62b4d8
alldo08 <69772003+alldo08@users.noreply.github.com>
2026-02-21 01:19:30 -0300

    Create firebase-messaging-sw.js

---
+++ site/static/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AcbXX3KgvqD7B8Y4WjCu6yNx1Prfu5cNHz",
+    projectId: "projeto-web-app-16adb",
+    messagingSenderId: "1011089630045",
+    appId: "1:1011089630045:web:3afc16a9817f11409c7eb4"
+});
+
+const messaging = firebase.messaging();
+
+// Opcional: Tratar mensagens em segundo plano
+messaging.onBackgroundMessage((payload) => {
+    console.log('Mensagem recebida em segundo plano:', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/static/icon.png'
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
