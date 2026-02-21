3d1f5be2bf023a78f96d0bcdac0bcefe7e2864d8
alldo08 <69772003+alldo08@users.noreply.github.com>
2026-02-21 01:42:07 -0300

    Update firebase-messaging-sw.js

--- site/firebase-messaging-sw.js
+++ site/firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AcbXX3KgvqD7B8Y4WjCu6yNx1Prfu5cNHz",
+    apiKey: "AIzaSyCGkSCkznXVBanRx5gDgeVSRS2mJU90UT8",
     projectId: "projeto-web-app-16adb",
     messagingSenderId: "1011089630045",
     appId: "1:1011089630045:web:3afc16a9817f11409c7eb4"
